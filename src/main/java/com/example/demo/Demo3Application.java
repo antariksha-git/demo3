d149e9a2baebe4fa52a4322e7e00421cef7592f2
antariksha-git <heyantariksha@gmail.com>
2024-10-20 14:49:45 +0530

    Setup completed

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo3Application {
-
+    //setup completed
     public static void main(String[] args) {
         SpringApplication.run(Demo3Application.class, args);
     }
